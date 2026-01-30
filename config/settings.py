813107e4d5015b715d4f62d2c59b481e04bb75d5
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-01-30 11:54:45 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -49,9 +49,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3002",
     "http://localhost:3003",
     "http://localhost:3004",
-    "https://45.55.129.34",
     "https://45.138.158.158",
-    "https://185.191.141.213"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -60,9 +58,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3002",
     "http://localhost:3003",
     "http://localhost:3004",
-    "https://45.55.129.34",
     "https://45.138.158.158",
-    "https://185.191.141.213"
 ]
 
 MIDDLEWARE = [
