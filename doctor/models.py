6e642222d22bf0e5835bb92b2ba84be6257cf81c
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-02-18 20:32:44 +0500

    done

--- doctor/models.py
+++ doctor/models.py
@@ -6,18 +6,18 @@ from category.utils import check_image_size
 class Doctor(models.Model):
     full_name = models.CharField(max_length=100)
     profession = models.CharField(max_length=100)
-    experience = models.IntegerField()
-    institution = models.CharField(max_length=200)
+    experience = models.IntegerField(null=True, blank=True)
+    institution = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(upload_to='doctor/', validators=[FileExtensionValidator(
         allowed_extensions=['jpg', 'jpeg', 'png', 'svg', 'webp', 'JPG', 'JPEG', 'PNG', 'SVG', 'WEBP']),
         check_image_size], blank=True, null=True)
     specialties = models.JSONField(blank=True, null=True)
-    education = models.JSONField(max_length=200)
+    education = models.JSONField(max_length=200, null=True, blank=True)
     works = models.JSONField(null=True, blank=True)
     services = models.JSONField(null=True, blank=True)
     achievements = models.JSONField(null=True, blank=True)
     reception = models.JSONField(null=True, blank=True)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20, null=True, blank=True)
     full_bio = models.TextField(null=True, blank=True)
 
     def __str__(self):
